8a1c9db8e59afd4a61e21f02a2e95e51f58954b9
Paraprax <parkerjohnston28@gmail.com>
2019-02-18 04:53:55 -0500

    crude but finally fully-working version of the algorithm that compares user survey answers to all the other profiles and gets a difference-number

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,11 +16,42 @@ module.exports = function(app) {
         var userAnswers = req.body;
         friendSurveys.push(req.body);
         console.log(friendSurveys);
-        for (var f = 0; f < friendSurveys.length; f++) {
-            
+        var sumOfDifferences = 0;
+        
+        for (var i = 0; i < friendSurveys.length; i++) { //for each profile in the friendSurveys array....
 
+            if (friendSurveys[i].name != userAnswers.name) { //exclude the user's own profile from the comparison(this would be less fallible if every user had a unique id)
+
+                console.log("bongo banana"); //test
+                console.log(userAnswers.scores[i]);
+                var allDifferences = [];
+                var difference = 0;
+
+                for (var j = 0; j < friendSurveys[i].scores.length; j++){ //for each number in each profile's score....
+                    
+                    if (userAnswers.scores[j] > friendSurveys[i].scores[j]) {
+                        difference = (userAnswers.scores[j] - friendSurveys[i].scores[j]); //subtract the profile-number from the user-number for the same question and set the result as a var called 'difference'
+                        console.log(difference + "alpha");
+                    }
+                    else {
+                        difference = (friendSurveys[i].scores[j] - userAnswers.scores[j]); //subtract the profile-number from the user-number for the same question and set the result as a var called 'difference'
+                        console.log(difference + "beta");
+                    }
+                    
+                    allDifferences.push(difference); //collect all the differences in one array
+                    console.log(allDifferences);
+                }
+
+                console.log(friendSurveys[i].name);
+
+                for (var k = 0; k < allDifferences.length; k++) { //loop through the array of differences and sum them into one number
+                    sumOfDifferences += allDifferences[k];
+                }
+
+                console.log("You are " + sumOfDifferences + " points different from " + friendSurveys[i].name);
+            }
         }
 
-        console.log("compatibility algoritihm coming soon...."); //TODO: write logic for the compatibility algoritihm
+        console.log("compatibility algoritihm coming soon...."); //TODO: finish logic for the compatibility algoritihm
     });
 }
